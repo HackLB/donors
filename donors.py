21a1c5b35ff733f74eff1124d3379013ffe9a415
Roger Howard <rogerhoward@mac.com>
2016-11-29 19:11:21 -0800

    Slight refactor
    * rename rec to record
    * cleanup imports
    * add requirements.txt

--- donors.py
+++ donors.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import os, sys
-import requests
 import simplejson as json
 from pprint import pprint
 import argparse
@@ -30,11 +29,11 @@ def save_donor_record(record):
     return path
 
 
-def cleanup(rec):
-    rec['AMNDT_IND'] = {'N': 'new', 'A': 'amendment', 'T': 'termination'}.get(rec['AMNDT_IND'])
+def cleanup(record):
+    record['AMNDT_IND'] = {'N': 'new', 'A': 'amendment', 'T': 'termination'}.get(record['AMNDT_IND'])
 
-    rec['RPT_TP'] = {'12C': 'PRE-CONVENTION', '12G': 'PRE-GENERAL', '12P': 'PRE-PRIMARY', '12R': 'PRE-RUN-OFF', '12S': 'PRE-SPECIAL', '30D': 'POST-ELECTION', '30G': 'POST-GENERAL', '30P': 'POST-PRIMARY', '30R': 'POST-RUN-OFF', '30S': 'POST-SPECIAL', '60D': 'POST-CONVENTION', 'ADJ': 'COMP ADJUST AMEND', 'CA': 'COMPREHENSIVE AMEND', 'M10': 'OCTOBER MONTHLY', 'M11': 'NOVEMBER MONTHLY', 'M12': 'DECEMBER MONTHLY', 'M2': 'FEBRUARY MONTHLY', 'M3': 'MARCH MONTHLY', 'M4': 'APRIL MONTHLY', 'M5': 'MAY MONTHLY', 'M6': 'JUNE MONTHLY', 'M7': 'JULY MONTHLY', 'M8': 'AUGUST MONTHLY', 'M9': 'SEPTEMBER MONTHLY', 'MY': 'MID-YEAR REPORT', 'Q1': 'APRIL QUARTERLY', 'Q2': 'JULY QUARTERLY', 'Q3': 'OCTOBER QUARTERLY', 'TER': 'TERMINATION REPORT', 'YE': 'YEAR-END', '90S': 'POST INAUGURAL SUPPLEMENT', '90D': 'POST INAUGURAL', '48H': '48 HOUR NOTIFICATION', '24H': '24 HOUR NOTIFICATION'}.get(rec['RPT_TP'])
-    return rec
+    record['RPT_TP'] = {'12C': 'PRE-CONVENTION', '12G': 'PRE-GENERAL', '12P': 'PRE-PRIMARY', '12R': 'PRE-RUN-OFF', '12S': 'PRE-SPECIAL', '30D': 'POST-ELECTION', '30G': 'POST-GENERAL', '30P': 'POST-PRIMARY', '30R': 'POST-RUN-OFF', '30S': 'POST-SPECIAL', '60D': 'POST-CONVENTION', 'ADJ': 'COMP ADJUST AMEND', 'CA': 'COMPREHENSIVE AMEND', 'M10': 'OCTOBER MONTHLY', 'M11': 'NOVEMBER MONTHLY', 'M12': 'DECEMBER MONTHLY', 'M2': 'FEBRUARY MONTHLY', 'M3': 'MARCH MONTHLY', 'M4': 'APRIL MONTHLY', 'M5': 'MAY MONTHLY', 'M6': 'JUNE MONTHLY', 'M7': 'JULY MONTHLY', 'M8': 'AUGUST MONTHLY', 'M9': 'SEPTEMBER MONTHLY', 'MY': 'MID-YEAR REPORT', 'Q1': 'APRIL QUARTERLY', 'Q2': 'JULY QUARTERLY', 'Q3': 'OCTOBER QUARTERLY', 'TER': 'TERMINATION REPORT', 'YE': 'YEAR-END', '90S': 'POST INAUGURAL SUPPLEMENT', '90D': 'POST INAUGURAL', '48H': '48 HOUR NOTIFICATION', '24H': '24 HOUR NOTIFICATION'}.get(record['RPT_TP'])
+    return record
 
 
 if __name__ == "__main__":
